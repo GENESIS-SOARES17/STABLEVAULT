765a957862323c7b496ca26eed933c5d5532e859
GENESIS-SOARES17 <edgardsilvasoares@gmail.com>
2026-06-10 19:26:18 -0300

    fix: correct STAKE_ABI with getUserActiveStakes and update contract address

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,36 +1,37 @@
-export const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || "0x0000000000000000000000000000000000000000";
-export const STAKE_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_STAKE_CONTRACT_ADDRESS || "0x0000000000000000000000000000000000000000";
+export const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || "0x0";
+export const STAKE_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_STAKE_CONTRACT_ADDRESS || "0x0";
 export const LTUSDC_ADDRESS = process.env.NEXT_PUBLIC_LTUSDC_ADDRESS || "0x5cCB920a9002740Af8F87dBeF52eF6394DD9bf35";
 
 export const CONTRACT_ABI = [
-  {"inputs":[{"internalType":"address","name":"_ltusdc","type":"address"},{"internalType":"uint256","name":"_airdropAmount","type":"uint256"},{"internalType":"uint256","name":"_mintAmount","type":"uint256"},{"internalType":"uint256","name":"_mintFee","type":"uint256"},{"internalType":"uint256","name":"_monthlyRateBps","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"},
-  {"inputs":[],"name":"claimAirdrop","outputs":[],"stateMutability":"nonpayable","type":"function"},
-  {"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"deposit","outputs":[],"stateMutability":"nonpayable","type":"function"},
-  {"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"getAirdropStatus","outputs":[{"internalType":"bool","name":"claimed","type":"bool"}],"stateMutability":"view","type":"function"},
-  {"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"getDepositInfo","outputs":[{"internalType":"uint256","name":"principal","type":"uint256"},{"internalType":"uint256","name":"interest","type":"uint256"},{"internalType":"uint256","name":"total","type":"uint256"},{"internalType":"uint256","name":"depositTime","type":"uint256"}],"stateMutability":"view","type":"function"},
-  {"inputs":[],"name":"getTotalDepositors","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
-  {"inputs":[],"name":"getTotalValueLocked","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
-  {"inputs":[],"name":"mintLTUSDC","outputs":[],"stateMutability":"payable","type":"function"},
-  {"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"partialWithdraw","outputs":[],"stateMutability":"nonpayable","type":"function"},
-  {"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}
+  "function claimAirdrop()",
+  "function mintLTUSDC() payable",
+  "function deposit(uint256)",
+  "function partialWithdraw(uint256)",
+  "function withdraw()",
+  "function getDepositInfo(address) view returns (uint256, uint256, uint256, uint256)",
+  "function getTotalValueLocked() view returns (uint256)",
+  "function getTotalDepositors() view returns (uint256)",
+  "function getAirdropStatus(address) view returns (bool)",
+  "function airdropAmount() view returns (uint256)",
+  "function mintAmount() view returns (uint256)",
+  "function mintFee() view returns (uint256)"
 ];
 
 export const STAKE_ABI = [
-  {"inputs":[{"internalType":"address","name":"_ltusdc","type":"address"},{"internalType":"uint256","name":"_monthlyRateBps","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"},
-  {"inputs":[{"internalType":"uint256","name":"_amount","type":"uint256"},{"internalType":"uint256","name":"_duration","type":"uint256"}],"name":"stake","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},
-  {"inputs":[{"internalType":"uint256","name":"_stakeId","type":"uint256"}],"name":"unstake","outputs":[],"stateMutability":"nonpayable","type":"function"},
-  {"inputs":[{"internalType":"uint256","name":"_stakeId","type":"uint256"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"partialUnstake","outputs":[],"stateMutability":"nonpayable","type":"function"},
-  {"inputs":[{"internalType":"uint256","name":"_stakeId","type":"uint256"}],"name":"calculateInterest","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
-  {"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"getUserStakes","outputs":[{"components":[{"internalType":"uint256","name":"id","type":"uint256"},{"internalType":"address","name":"user","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"uint256","name":"stakeTime","type":"uint256"},{"internalType":"uint256","name":"duration","type":"uint256"},{"internalType":"uint256","name":"rewardRate","type":"uint256"},{"internalType":"bool","name":"active","type":"bool"}],"internalType":"struct StakeVault.Stake[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},
-  {"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"getUserActiveStakes","outputs":[{"components":[{"internalType":"uint256","name":"id","type":"uint256"},{"internalType":"address","name":"user","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"uint256","name":"stakeTime","type":"uint256"},{"internalType":"uint256","name":"duration","type":"uint256"},{"internalType":"uint256","name":"rewardRate","type":"uint256"},{"internalType":"bool","name":"active","type":"bool"}],"internalType":"struct StakeVault.Stake[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},
-  {"inputs":[{"internalType":"uint256","name":"_stakeId","type":"uint256"}],"name":"getStakeInfo","outputs":[{"internalType":"uint256","name":"id","type":"uint256"},{"internalType":"address","name":"user","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"uint256","name":"stakeTime","type":"uint256"},{"internalType":"uint256","name":"duration","type":"uint256"},{"internalType":"uint256","name":"rewardRate","type":"uint256"},{"internalType":"bool","name":"active","type":"bool"}],"stateMutability":"view","type":"function"},
-  {"inputs":[],"name":"getTotalStakers","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
-  {"inputs":[],"name":"getTotalStaked","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
-  {"inputs":[],"name":"ltusdc","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"}
+  "function stake(uint256, uint256) returns (uint256)",
+  "function unstake(uint256)",
+  "function partialUnstake(uint256, uint256)",
+  "function calculateInterest(uint256) view returns (uint256)",
+  "function getUserActiveStakes(address) view returns ((uint256,address,uint256,uint256,uint256,uint256,bool)[])",
+  "function getUserStakes(address) view returns ((uint256,address,uint256,uint256,uint256,uint256,bool)[])",
+  "function getStakeInfo(uint256) view returns (uint256,address,uint256,uint256,uint256,uint256,bool)",
+  "function getTotalStaked() view returns (uint256)",
+  "function getTotalStakers() view returns (uint256)",
+  "function ltusdc() view returns (address)"
 ];
 
 export const ERC20_ABI = [
-  {"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},
-  {"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
-  {"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}
+  "function approve(address, uint256) returns (bool)",
+  "function balanceOf(address) view returns (uint256)",
+  "function allowance(address, address) view returns (uint256)"
 ];
